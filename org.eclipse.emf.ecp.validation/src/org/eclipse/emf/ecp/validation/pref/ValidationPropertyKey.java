f6c2cc537e0d570813f108eb4a984bbf50bd3aef
jonas <jonas.helming@googlemail.com>
2011-09-14 17:53:17 +0200

    added new validation plugin which does not use the emf validation framework anymore but the EMF build in validation

---
+++ org.eclipse.emf.ecp.validation/src/org/eclipse/emf/ecp/validation/pref/ValidationPropertyKey.java
@@ -0,0 +1,10 @@
+//Disabled until bug 353491 is fixed 
+//package org.eclipse.emf.ecp.validation.pref;
+//
+//import org.eclipse.emf.emfstore.client.model.preferences.PropertyKey;
+//
+//
+//public enum ValidationPropertyKey implements PropertyKey{
+//
+//	ENABLELIVEVALIDATION,
+//}
