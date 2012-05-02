abda4ea146c8c21ae90b0274206d67b2e2353511
mhohenegger <mhohenegger@eclipsesource.com>
2012-05-02 11:49:29 +0200

    fixed header

--- org.eclipse.emf.ecp.editor/src/org/eclipse/emf/ecp/editor/mecontrols/MEPrimitiveAttributeControl.java
+++ org.eclipse.emf.ecp.editor/src/org/eclipse/emf/ecp/editor/mecontrols/MEPrimitiveAttributeControl.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors:
+ * Contributors: Max Hohenegger (Bug 377561)
  ******************************************************************************/
 package org.eclipse.emf.ecp.editor.mecontrols;
 
